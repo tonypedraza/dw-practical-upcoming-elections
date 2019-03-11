fd2fb79e3ae196b8e39d6c70935afd6dacd8180c
Luis Pedraza <luispedraza@utexas.edu>
2019-03-11 15:40:39 -0400

    added node-fetch; generating api url and contacting turbovote

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var us_states = require('../us_state.js');
+const fetch = require('node-fetch');
+
+function getUrl(state, city) {
+  // Add required state and city data to the Turbovote API url
+  let url = "https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:"
+  url += state.toLowerCase()
+  url += ",ocd-division/country:us/state:"
+  url += state.toLowerCase() + "/place:" + city.toLowerCase().replace(/\s/g, "")
+  return url
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,13 +20,32 @@ router.get('/', function(req, res, next) {
 /* POST search endpoint */
 router.post('/search', function(req, res) {
   const data = req.body
-  if (!(data.state || data.zip)) {
-    let results = "You need to select a state or enter a zip code."
-    res.render('index', { title: 'Find My Election', states: us_states, results: results })
+  /* Would like to change the below conditional to pass if zip code entered,
+  but need an API key for Google's geocode API to get state from zip code. */
+  if (!(data.state || data.city)) {
+    res.render('index', { title: 'Find My Election', states: us_states, error: "You need enter a city and select a state." })
   }
   else {
-    let results = "We have a state or zip code."
-    res.render('index', { title: 'Find My Election', states: us_states, results: results })
+    let url = getUrl(data.state, data.city)
+    // Fetch data from Turbovote and then send to index template or send error message to index template
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+      }
+    })
+    .then(response => response.json())
+    .then(response_json => {
+      if (response_json.length === 0) {
+        res.render('index', { title: 'Find My Election', states: us_states, error: "There are no results for your city and state." })
+      }
+      else {
+        res.render('index', { title: 'Find My Election', states: us_states, results: response_json })
+      }
+    })
+    .catch( error => {
+      res.render('index', { title: 'Find My Election', states: us_states, error: "There was an error processing your request." })
+    })
   }
 })
 
