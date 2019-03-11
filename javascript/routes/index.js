35ac746171f2b8336632195eb27980834669bb65
Luis Pedraza <luispedraza@utexas.edu>
2019-03-11 14:30:37 -0400

    set up search endpoint skeleton

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -7,4 +7,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Find My Election', states: us_states });
 });
 
+/* POST search endpoint */
+router.post('/search', function(req, res) {
+  const data = req.body
+  if (!(data.state || data.zip)) {
+    let results = "You need to select a state or enter a zip code."
+    res.render('index', { title: 'Find My Election', states: us_states, results: results })
+  }
+  else {
+    let results = "We have a state or zip code."
+    res.render('index', { title: 'Find My Election', states: us_states, results: results })
+  }
+})
+
 module.exports = router;
